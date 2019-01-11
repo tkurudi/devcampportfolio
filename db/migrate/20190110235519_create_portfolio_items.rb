7ca03874ca910976e82ea54fa043d7b568cde98d
tkurudi <t.kurudi209@gmail.com>
2019-01-11 13:03:51 +1300

    added portfolio gear

---
+++ db/migrate/20190110235519_create_portfolio_items.rb
@@ -0,0 +1,13 @@
+class CreatePortfolioItems < ActiveRecord::Migration[5.2]
+  def change
+    create_table :portfolio_items do |t|
+      t.string :title
+      t.string :subtitle
+      t.string :body
+      t.text :main_image
+      t.text :thumb_image
+
+      t.timestamps
+    end
+  end
+end
