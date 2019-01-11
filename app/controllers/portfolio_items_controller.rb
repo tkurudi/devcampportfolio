9a1c567e72b131d4c138c20cac0485d318377020
tkurudi <t.kurudi209@gmail.com>
2019-01-11 15:29:54 +1300

    added a form and a 'new' view

--- app/controllers/portfolio_items_controller.rb
+++ app/controllers/portfolio_items_controller.rb
@@ -2,4 +2,21 @@ class PortfolioItemsController < ApplicationController
     def index 
         @portfolio_items = PortfolioItem.all 
     end
+
+    def new
+        @portfolio_item = PortfolioItem.new
+    end
+    def create
+        @portfolio_item = PortfolioItem.new(params.require(:portfolio_item).permit(:title, :subtitle, :thumb_image, :main_image, :body))
+    
+        respond_to do |format|
+          if @portfolio_item.save
+            format.html { redirect_to portfolio_items_path, notice: 'Your portfilio item is now live.' }
+            format.json { render :show, status: :created, location: @portfolio_item }
+          else
+            format.html { render :new }
+            format.json { render json: @portfolio_item.errors, status: :unprocessable_entity }
+          end
+        end
+      end
 end
