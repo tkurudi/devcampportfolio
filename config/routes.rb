903de75c68166668204617b4c46801a30a632c8b
tkurudi <t.kurudi209@gmail.com>
2019-01-17 15:35:23 +1300

    auth stuff and routes for log in ect pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  resources :portfolio_items
+  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register'}
+  resources :portfolio_items, expect: [:show]
     
   
   get '/about', to: 'pages#about'
