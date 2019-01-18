f30f8795d8946e71bd79d01dcc825ec4fd0487ac
tkurudi <t.kurudi209@gmail.com>
2019-01-18 14:24:47 +1300

    added a devise concern and refactored the app controller

---
+++ app/controllers/concerns/devise_whitelist.rb
@@ -0,0 +1,13 @@
+module DeviseWhitelist
+    extend ActiveSupport::Concern
+
+    included do
+        before_action :configure_permitted_params, if: :devise_controller?
+    end
+
+    def configure_permitted_params
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+        devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+
+    end
+end
