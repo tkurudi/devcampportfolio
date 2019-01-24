abb97f6e2fb952743d34e0aebd8ec12074e8e46d
tkurudi <t.kurudi209@gmail.com>
2019-01-24 16:21:34 +1300

    added more helper things

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,8 +4,16 @@ module ApplicationHelper
             link_to "Logout", destroy_user_session_path, method: :delete
           else
           (link_to "Register", user_session_path) +
-          </br>.html_safe +
+          "<br>".html_safe +
               (link_to "Login", user_session_path)
           end
     end 
+
+    def set_source_helper(layout_name)
+        if session[:source]
+            greeting = "Thanks for visiting me from  #{session[:source]}"
+            content_tag(:P, greeting, class: "source-greeting")
+            end
+    end
+
 end
